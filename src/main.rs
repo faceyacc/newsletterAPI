b30c6418691bb4bab87278c612f36ae13b32cf2d
TY <ty@TYs-MacBook-Air.local>
2022-04-22 00:10:50 -0700

    updated main.rs to accept PGconnection

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 //! src/main.rs
-use std::net::TcpListener;
 use newsletterAPI::startup::run;
 use newsletterAPI::configuration::get_configuration;
+use std::net::TcpListener;
+use sqlx::{Connection, PgConnection};
 
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if it can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     
     // Port comes from settings
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, connection)?.await
+
 }
